114a43ff281441037320eb1a14111f3b6ab4195d
CodeEter <47411900+CodeEter@users.noreply.github.com>
2024-04-24 14:23:55 -0400

    Update async.js
    setTimeout 2 times

--- async.js
+++ async.js
@@ -3,3 +3,12 @@ setTimeout(myFunction, 3000);
 function myFunction() {
     console.log("I Love You !!");
 }
+
+
+//myFunction is used as a callback
+
+setTimeout(function () { myFunction1("I love You!!!"); }, 3000);
+
+function myFunction1(value) {
+    console.log(value);
+}
